5b17d22c93dbbd740d03bb29afe2f79ae62c28a1
Narval1 <matheushsm51@gmail.com>
2021-08-24 12:07:52 -0300

    alterado o packagejson

--- application/scr/api/controller.js
+++ application/scr/api/controller.js
@@ -1,10 +1,8 @@
-import { BadRequestError } from 'restify-errors'
-
-export const removeDuplicate = (req, res, next) => {
+export const removeDuplicate = (req, res) => {
   // gets the phrase and remove any blank space in the beginning or in the end
   const phrase = req.params.phrase.trim()
 
-  if (!phrase) return next(new BadRequestError('blank phrase'))
+  if (!phrase) return res.json({ error: 'blank phrase' })
 
   const array = phrase.split(' ')
   const newPhrase = array.filter((element, index) => array.indexOf(element) === index)
@@ -12,27 +10,23 @@ export const removeDuplicate = (req, res, next) => {
   res.json({ result: newPhrase.join(' ') })
 }
 
-export const changeVowals = (req, res, next) => {
+export const changeVowals = (req, res) => {
   // body validations
-  if (!req.body.letter.trim() && !req.body.phrase.trim()) return next(new BadRequestError('empty body'))
-  if (!req.body.phrase.trim()) return next(new BadRequestError('blank sentence'))
-  if (!req.body.letter.trim()) return next(new BadRequestError('inform the desired letter'))
+  if (!req.body.letter.trim() && !req.body.phrase.trim()) return res.json({ error: 'empty body' })
+  if (!req.body.phrase.trim()) return res.json({ error: 'blank sentence' })
+  if (!req.body.letter.trim()) return res.json({ error: 'inform the desired letter' })
 
   // gets the phrase and the letter without blank spaces in the beginning or in the end
-  const phrase = req.body.phrase.trim().split('')
+  const phrase = req.body.phrase.trim()
   const letter = req.body.letter.trim()
 
   // check if the the letter is a character
-  if (letter.length > 1) return next(new BadRequestError('enter only one character'))
+  if (letter.length > 1) return res.json({ error: 'enter only one character' })
 
   // change all vowels
-  for (const i in phrase) {
-    if (phrase[i] === 'a' || phrase[i] === 'e' || phrase[i] === 'i' || phrase[i] === 'o' || phrase[i] === 'u') {
-      phrase[i] = letter
-    }
-  }
+  const newPhrase = phrase.replace(/[aeiou]/ig, letter)
 
-  res.send({ result: phrase.join('') })
+  res.send({ result: newPhrase })
 }
 
 export default {
